4a9266072060a145d52be26055e99b9593201ed4
shivam-26 <79416882+shivam-26@users.noreply.github.com>
2021-12-30 20:12:36 +0530

    Keeper-App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render;
+ReactDOM.render(<App />, document.getElementById("root"));
 
-//1. Create a new React app.
 //2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
